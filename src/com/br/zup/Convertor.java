2bb900d29b627024388cf18eb0f795a934a4b399
natalia.palota <natalia.palota@zup.com.br>
2021-02-05 13:54:44 -0300

    Converter reais para dolares

---
+++ src/com/br/zup/Convertor.java
@@ -0,0 +1,24 @@
+package com.br.zup;
+
+
+import java.text.DecimalFormat;
+
+class Real {
+    private double reias;
+
+    public Real(double reais){
+        this.reias = reais;
+    }
+
+    public double converterParaDolar(){
+        double resultado = this.reias / 5.35;
+        DecimalFormat formatador = new DecimalFormat("#.##");
+        String valorFormatado = formatador.format(resultado).replace(",", ".");
+        return Double.valueOf(valorFormatado);
+    }
+
+    public double getReias() {
+        return reias;
+    }
+}
+
